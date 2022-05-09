0e0328369541a70432ea996efa9441f2a23539b2
lenin@gmail.com <lenin@gmail.com>
2022-05-09 16:23:31 +0530

    server

--- controllers/admin/new_user.js
+++ controllers/admin/new_user.js
@@ -1,7 +1,6 @@
 const { pool } = require("../../db/db");
 
 exports.Assign_role = async (req, res) => {
-    let success = {};
     let errors = {};
     let body = req.body;
     let bodykeys = Object.keys(body);
@@ -16,29 +15,35 @@ exports.Assign_role = async (req, res) => {
             errors.action.push('Unable to parse request body. ' + err.message);
         }
     }
-    let type;
+    let role;
     if (Object.keys(errors).length === 0) {
-        if (bodykeys.includes('type')) {
-            type = req.body.type;
-            if (!type) {
-                errors.type = [];
-                errors.type.push(`Type is required`);
+        if (bodykeys.includes('role')) {
+            role = req.body.role;
+            if (!role) {
+                errors.role = [];
+                errors.role.push(`role is required`);
             }
         } else {
+            errors.role = [];
+            errors.role.push(`role should be provided in the request body`)
+        }
+    }
+    if (Object.keys(errors).length === 0) {
+        try {
+            await pool.query(`INSERT INTO assignrole (roles) VALUES ('${type}');`, function (error, results, fields) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    return res.status(200).json(results)
+                }
+            });
+
+        } catch (err) {
             errors.type = [];
             errors.type.push(`type should be provided in the request body`)
         }
     }
     if (Object.keys(errors).length === 0) {
-        try{
-            await pool.query(`INSERT INTO assignrole (types) VALUES ('${type}');`, function (error, results, fields) {
-                if (error) {
-                    console.log(error);
-                }
-                res.status(200).json(JSON.parse(JSON.stringify(data)));
-            });
-        }catch(err){
-
-        }
+        res.status(400).json(errors)
     }
 }
--- route/admin.js
+++ route/admin.js
@@ -1,5 +1,6 @@
 const Router = require('express').Router();
 const { customer_types, get_customer_types,nationality, get_nationality,oldclient, get_oldclient,restricted, get_restricted_types,vip, get_vip } = require('../controllers/admin/customer_information');
+const { Assign_role } = require('../controllers/admin/new_user');
 Router.post('/customer_types',customer_types);
 Router.get('/customer_types',get_customer_types);
 Router.post('/restricted',restricted);
@@ -10,4 +11,5 @@ Router.post('/oldclient',oldclient);
 Router.get('/oldclient',get_oldclient);
 Router.post('/vip',vip);
 Router.get('/vip',get_vip);
+Router.post('/assign_role',Assign_role)
 module.exports = Router;
